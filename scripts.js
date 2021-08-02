5c6164db17b750e11d5cdd53ce9d3fded5720f43
Ephie Adhiambo <ephie.adhiambo@student.moringaschool.com>
2021-08-02 15:13:47 +0300

    final work

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ function getSize() {
   return parseInt(size);
 }
 function getFlavour() {
-  var flavour= document.getElementById("flavour").value;
+  var flavour = document.getElementById("flavour").value;
   return parseInt(flavour);
 }
 function getCrust() {
@@ -19,13 +19,15 @@ function getQuantity() {
   return parseInt(quantity);
 }
 function totalAmount() {
-  var totals = (getSize() +getFlavour() + getCrust() + getToppings()) * getQuantity();
+  var totals = (getSize() + getFlavour() + getCrust() + getToppings()) * getQuantity();
   alert(
-    "We have recieved your order." + "Total price is 2300 "+
+    "We have recieved your order." + "Total price is 2300. " +
     "Thank you for making an order!"
   );
-
- 
+  
+  // if (totals !==" "){
+  //   alert("select a choice in every field below!")
+  // }
   var location = prompt("For Delivery, please put in your location");
   if (location !== "") {
     alert("Delivery Fee is 200.Your Order will be Delivered within 20-30mins");
